0f15f05aa28cad86b63833b2e7df110da23665f0
hscheuerle <h.a.scheuerle@gmail.com>
2019-10-28 16:29:00 -0400

    button to dev the dispatch functionality

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+
+type User = any;
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +10,21 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  users$: Observable<User[]> = this.store.select(state => state.github.users);
 
-  constructor() {}
+  constructor(
+    private store: Store<{ github: { users: User[] } }>
+    ) {
+  }
 
+  loadData(event) {
+    console.log(event);
+  }
+
+  loadMoreUsers() {
+    this.store.dispatch({ type: '[Feed Page] Load Users'});
+  }
+
+  ionViewDidEnter() {
+  }
 }
