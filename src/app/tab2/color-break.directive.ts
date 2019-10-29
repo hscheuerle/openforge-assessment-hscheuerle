9dcf199f6352dae181a799a94333105b63a3b3ec
hscheuerle <h.a.scheuerle@gmail.com>
2019-10-29 18:34:56 -0400

    color break directive

--- src/app/tab2/color-break.directive.ts
+++ src/app/tab2/color-break.directive.ts
@@ -1,10 +1,18 @@
-import { Directive } from '@angular/core';
+import { Directive, Input, ElementRef, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appColorBreak]'
 })
-export class ColorBreakDirective {
+export class ColorBreakDirective implements OnInit {
+  @Input() appColorBreak: boolean;
 
-  constructor() { }
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
 
+  ngOnInit() {
+    if (this.appColorBreak) {
+      this.renderer.setAttribute(this.el.nativeElement, 'color', 'secondary');
+    } else {
+      this.renderer.setAttribute(this.el.nativeElement, 'color', 'primary');
+    }
+  }
 }
