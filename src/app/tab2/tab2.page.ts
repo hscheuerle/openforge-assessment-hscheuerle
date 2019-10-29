acfb55d198c9fe70690d2aeed5c123c08693840e
hscheuerle <h.a.scheuerle@gmail.com>
2019-10-29 08:54:19 -0400

    makeshift queryparam solution, move to data depending on ionic implementation

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
 
 class Profile {
   constructor() {
@@ -13,12 +14,25 @@ class Profile {
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
+  value = '';
   selectedUser$: Observable<object> = this.store.select(state => state.github.selectedUser);
 
   constructor(
-    private store: Store<{ github: { selectedUser: object } }>
-  ) {}
+    private store: Store<{ github: { selectedUser: object } }>,
+    private route: ActivatedRoute,
+  ) {
+    const search = this.route.snapshot.queryParamMap.get('search');
+    console.log(search);
+    if (search) { this.value = search; }
+  }
+
+  ngOnInit() {
+    if (this.value) {
+      // change to Get User?
+      this.store.dispatch({ type: '[Github API] Search User', props: { input: this.value }});
+    }
+  }
 
   searchUser(event) {
     const { value } = event.target;
