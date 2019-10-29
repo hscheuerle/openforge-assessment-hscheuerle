1bb0046cc33411ba5fb01f724378c67c74c2bac0
hscheuerle <h.a.scheuerle@gmail.com>
2019-10-29 13:32:32 -0400

    type fixing

--- src/app/actions/github.actions.ts
+++ src/app/actions/github.actions.ts
@@ -1,5 +1,26 @@
 import { createAction, props } from '@ngrx/store';
 
+export interface UsersPayload {
+    login: string;
+    id: number;
+    node_id: string;
+    avatar_url: string;
+    gravatar_id: string;
+    url: string;
+    html_url: string;
+    followers_url: string;
+    following_url: string;
+    gists_url: string;
+    starred_url: string;
+    subscriptions_url: string;
+    organizations_url: string;
+    repos_url: string;
+    events_url: string;
+    received_events_url: string;
+    type: string;
+    site_admin: boolean;
+}
+
 export interface UserPayload {
     total_count: number;
     incomplete_results: boolean;
@@ -32,8 +53,9 @@ export interface UserPayload {
 }
 
 export const loadUsers = createAction('[Github API] Load Users');
-export const usersLoadedSuccess = createAction('[Github API] Users Loaded Success', props<{ payload: { users: [], since: string } }>());
-export const searchUser = createAction('[Github API] Search User', props<{ props: { input: string }}>());
+export const usersLoadedSuccess = createAction('[Github API] Users Loaded Success',
+    props<{ payload: { users: UsersPayload[], since: string } }>());
+export const searchUser = createAction('[Github API] Search User', props<{ props: { input: string } }>());
 export const searchUserSuccess = createAction('[Github API] Search User Success', props<{ payload: UserPayload }>());
 export const getUserSuccess = createAction('[Github API] Get User Success', props<{ payload: any }>());
 export const clearUser = createAction('[Github API] Clear User');
